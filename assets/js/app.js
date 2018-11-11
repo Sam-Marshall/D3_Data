f42fa5ccf9a329551a76cd7718b224c603d42c18
sam-marshall <stacy.marshall@northwestern.edu>
2018-11-11 13:06:25 -0600

    added basic height/width/svg info and read in csv file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,31 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 660;
+var data = '../../data/data.csv';
+
+var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+};
+
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth)
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+d3.csv("../../data/data.csv").then(function(stateData) {
+    stateData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.income = +data.income;
+        data.healthcare = +data.healthcare;
+    });
+    console.log(stateData);
+});
